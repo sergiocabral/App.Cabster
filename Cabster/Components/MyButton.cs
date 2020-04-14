0d6710c487a758d0d210deb7bfee19139238b480
Sergio Cabral <sergio@cabral.br.com>
2020-04-14 07:02:12 -0300

    Removido borda dos botões.

--- Cabster/Components/MyButton.cs
+++ Cabster/Components/MyButton.cs
@@ -60,6 +60,11 @@ namespace Cabster.Components
         }
 
         /// <summary>
+        ///     Exibe ou não retângulo quando tem o foco.
+        /// </summary>
+        protected override bool ShowFocusCues => false;
+
+        /// <summary>
         ///     Evita transparência.
         /// </summary>
         public bool NotTransparent { get; set; }
@@ -119,6 +124,7 @@ namespace Cabster.Components
         public void UpdateLayout()
         {
             _buttonIsIcon = Images.ContainsKey(Name);
+            FlatAppearance.BorderColor = Color.FromArgb(0, 255, 255, 255); 
             if (_buttonIsIcon)
             {
                 var (bitmapLeave, bitmapEnter) = Images[Name];
